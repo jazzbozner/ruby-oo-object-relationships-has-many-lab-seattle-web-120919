5b91b5129e681f30320dccb702e71b2d50af15bf
github username <github email address>
2019-12-16 11:27:39 -0800

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -23,11 +23,13 @@ class Author
 
     def add_post_by_title(title)
         # binding.pry
-        self.add_post(Post.new(title))
+        # self.add_post(Post.new(title))
+        # new_post = Post.new(title)
+        # new_post.author = self
     end
 
     def self.post_count
         Post.all.length
     end
-    
+
 end
